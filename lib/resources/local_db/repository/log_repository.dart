c26c9365e0b8d0d21b5d6ba7fd6502d7a845ae1e
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-11-25 11:17:44 +0530

    CRUD Hive

---
+++ lib/resources/local_db/repository/log_repository.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:spag_connect/models/log.dart';
+import 'package:spag_connect/resources/local_db/db/hive_methods.dart';
+import 'package:spag_connect/resources/local_db/db/sqlite_methods.dart';
+
+class LogRepository {
+  static var dbObject;
+  static bool isHive;
+
+  static init({@required bool isHive}) {
+    dbObject = isHive ? HiveMethods() : SqliteMethods();
+    dbObject.init();
+  }
+
+  static addLogs(Log log) => dbObject.addLogs(log);
+
+  static deleteLogs(int logId) => dbObject.deleteLogs(logId);
+
+  static getLogs() => dbObject.getLogs();
+
+  static close() => dbObject.close();
+}
