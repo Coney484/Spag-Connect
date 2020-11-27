c04185dc735afa4033251ccab008f55f74feb1cb
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-11-27 08:42:08 +0530

    final commit

--- lib/screens/callscreens/pickup/pickup_layout.dart
+++ lib/screens/callscreens/pickup/pickup_layout.dart
@@ -10,23 +10,27 @@ class PickUpLayout extends StatelessWidget {
   final Widget scaffold;
   final CallMethods callMethods = CallMethods();
 
-  PickUpLayout({@required this.scaffold});
+  PickUpLayout({
+    @required this.scaffold,
+  });
+
   @override
   Widget build(BuildContext context) {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
+
     return (userProvider != null && userProvider.getUser != null)
         ? StreamBuilder<DocumentSnapshot>(
             stream: callMethods.callStream(uid: userProvider.getUser.uid),
             builder: (context, snapshot) {
               if (snapshot.hasData && snapshot.data.data != null) {
                 Call call = Call.fromMap(snapshot.data.data);
+
                 if (!call.hasDialled) {
                   return PickUpScreen(call: call);
                 }
               }
               return scaffold;
-            },
-          )
+            })
         : Scaffold(
             body: Center(
               child: CircularProgressIndicator(),
--- lib/screens/callscreens/pickup/pickup_screen.dart
+++ lib/screens/callscreens/pickup/pickup_screen.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:spag_connect/constants/strings.dart';
 import 'package:spag_connect/models/call.dart';
+import 'package:spag_connect/models/log.dart';
 import 'package:spag_connect/resources/call_methods.dart';
+import 'package:spag_connect/resources/local_db/repository/log_repository.dart';
 import 'package:spag_connect/screens/callscreens/call_screen.dart';
 import 'package:spag_connect/screens/chatscreens/widgets/cached_image.dart';
 import 'package:spag_connect/utils/permission.dart';
 
-class  PickUpScreen extends StatelessWidget {
+class PickUpScreen extends StatefulWidget {
   final Call call;
-  final CallMethods callMethods = CallMethods();
 
   PickUpScreen({
     @required this.call,
   });
+
+  @override
+  _PickUpScreenState createState() => _PickUpScreenState();
+}
+
+class _PickUpScreenState extends State<PickUpScreen> {
+  final CallMethods callMethods = CallMethods();
+
+  bool isCallMissed = true;
+  // initializes and adds log to db
+  addToLocalStorage({@required String callStatus}) {
+    Log log = Log(
+      callerName: widget.call.callerName,
+      callerPic: widget.call.callerPic,
+      receiverName: widget.call.receiverName,
+      receiverPic: widget.call.receiverPic,
+      timestamp: DateTime.now().toString(),
+      callStatus: callStatus,
+    );
+
+    // adds the data to database
+    LogRepository.addLogs(log);
+  }
+
+  @override
+  void dispose() {
+    
+    if (isCallMissed) {
+      addToLocalStorage(callStatus: CALL_STATUS_MISSED);
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +64,7 @@ class  PickUpScreen extends StatelessWidget {
               height: 50,
             ),
             CachedImage(
-              call.callerPic,
+              widget.call.callerPic,
               isRound: true,
               radius: 180,
             ),
@@ -38,7 +72,7 @@ class  PickUpScreen extends StatelessWidget {
               height: 15,
             ),
             Text(
-              call.callerName,
+              widget.call.callerName,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
@@ -54,25 +88,30 @@ class  PickUpScreen extends StatelessWidget {
                   icon: Icon(Icons.call_end),
                   color: Colors.redAccent,
                   onPressed: () async {
-                    await callMethods.endCall(call);
+                    isCallMissed = false;
+                    addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                    await callMethods.endCall(widget.call);
                   },
                 ),
                 SizedBox(
                   width: 25,
                 ),
                 IconButton(
-                  icon: Icon(Icons.call),
-                  color: Colors.green,
-                  onPressed: () async =>
+                    icon: Icon(Icons.call),
+                    color: Colors.green,
+                    onPressed: () async {
+                      isCallMissed = false;
+                      addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
                       await Permissions.cameraAndMicrophonePermissionsGranted()
                           ? Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (context) => CallScreen(call: call),
+                                builder: (context) =>
+                                    CallScreen(call: widget.call),
                               ),
                             )
-                          : {},
-                ),
+                          : {};
+                    }),
               ],
             )
           ],
